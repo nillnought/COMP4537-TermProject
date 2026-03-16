acd26ad66036dc3f2dc61b5efa43c1b6abdbbc2e
nillnought <gglaim@my.bcit.ca>
2026-03-16 16:56:50 -0700

    Add toggle between registration and log in

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -1,9 +1,31 @@
 const usernameEle = document.getElementById("username");
 const passwordEle = document.getElementById("password");
 
+const inputWrap = document.getElementById("form-input-wrap");
+
+const switchEle = document.getElementById("switch");
+const forgotEle = document.getElementById("forgot");
+
 const submitBtn = document.getElementById("submit-btn");
 
+let LogIn = true;
+
 submitBtn.addEventListener("click", processForm);
+
+switchEle.addEventListener("click", switchLogIn);
+
+// Move to a Lang file later
+const LogInFields = `<input placeholder="Email" class="form-input" id="username"
+                    type="email" required>
+                    <input placeholder="Password" class="form-input" type="password" id="password" required>`;
+
+const SignUpFields = `<input placeholder="Email" class="form-input" id="username"
+                    type="email" required>
+                    <input placeholder="Password" class="form-input" type="password" id="password" required>
+                    <input placeholder="Confirm password" class="form-input" type="password" id="password" required>`;
+
+const LogInMsg = "Have an account?";
+const SignUpMsg = "Don't have an account?";
 
 function processForm(){
     const username = usernameEle.value;
@@ -11,4 +33,21 @@ function processForm(){
 
     console.log(username);
     console.log(password);
+}
+
+function validateLogin(username, password){
+// placeholder, will put checking logic here later
+}
+
+function switchLogIn(){
+    LogIn = !LogIn;
+    if(LogIn){
+        inputWrap.innerHTML = LogInFields;
+        switchEle.textContent = SignUpMsg;
+        forgotEle.style = "";
+    } else {
+        inputWrap.innerHTML = SignUpFields;
+        switchEle.textContent = LogInMsg;
+        forgotEle.style.display = "none";
+    }
 }
