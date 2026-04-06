466ad990e7ae52bfd044a937ee3c9d9a682dc323
Kanon Nishiyama <knishiyama@my.bcit.ca>
2026-04-05 21:51:09 -0700

    Remove and update models

--- backend/models/Score.js
+++ backend/models/Score.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const progressSchema = new mongoose.Schema({
+const scoreSchema = new mongoose.Schema({
   quizID: { type: Number, required: true, unique: true, index: true },
   studentID: { type: Number, required: true, index: true },
   completed: { type: Boolean, required: true, default: false },
-  answers: { type: [Number], required: true, default: [] },
+  score: { type: Number, required: true, default: [] },
 });
 
-module.exports = mongoose.model('Progress', progressSchema);
+module.exports = mongoose.model('Score', scoreSchema);
